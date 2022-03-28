58d7702aec84b40ba316119d954b5c34d17d3b8f
Victor Pena <victorpenawow@gmail.com>
2022-03-28 04:07:04 +0200

    Proyecto ejemplo y pruebaApp añadidos

---
+++ ejemplo/ejemplo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ejemplo
+//
+//  Created by victorpena on 28/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ pruebaApp/pruebaApp/pruebaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  pruebaAppApp.swift
+//  pruebaApp
+//
+//  Created by victorpena on 28/3/22.
+//
+
+import SwiftUI
+
+@main
+struct pruebaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
